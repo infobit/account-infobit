0eacffa5237e8bc40a571dac7a7849491e8bc60a
infobit <jorge@infobit.es>
2019-05-27 14:57:00 +0000

    eliminar contabilizacion tpv

---
+++ statement_from_point_of_sale/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Not registry asset from point of sale',
+    'version': '10.0.1.1.0',
+    'category': 'Accounting & Finance',
+    'summary': 'Not registry asset form point of sale',
+    'author': 'Infobit Informatica',
+    'license': 'AGPL-3',
+    'website': 'www.infobit.es',
+    'depends': ['point_of_sale'],
+    'data': [],
+    'installable': True,
+}
---
+++ statement_from_point_of_sale/pos_session.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, AccessError
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.misc import formatLang
+from odoo.addons import decimal_precision as dp
+
+class pos_session(models.Model):
+  _inherit ="pos.session"
+
+  @api.multi
+  def action_pos_session_close(self):
+        # Close CashBox
+        for session in self:
+            company_id = session.config_id.company_id.id
+            ctx = dict(self.env.context, force_company=company_id, company_id=company_id)
+            for st in session.statement_ids:
+                if abs(st.difference) > st.journal_id.amount_authorized_diff:
+                    # The pos manager can close statements with maximums.
+                    if not self.user_has_groups("point_of_sale.group_pos_manager"):
+                        raise UserError(_("Your ending balance is too different from the theoretical cash closing (%.2f), the maximum allowed is: %.2f. You can contact your manager to force it.") % (st.difference, st.journal_id.amount_authorized_diff))
+                if (st.journal_id.type not in ['bank', 'cash']):
+                    raise UserError(_("The type of the journal for your payment method should be bank or cash "))
+                #st.with_context(ctx).sudo().button_confirm_bank()
+        #self.with_context(ctx)._confirm_orders()
+        self.write({'state': 'closed'})
+        return {
+            'type': 'ir.actions.client',
+            'name': 'Point of Sale Menu',
+            'tag': 'reload',
+            'params': {'menu_id': self.env.ref('point_of_sale.menu_point_root').id},
+        }
+
